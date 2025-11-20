ce2bad61d8913b285d446b5d70a4103004944c9f
wenjun-1028 <hwj3838438@163.com>
2025-11-20 14:08:58 +0800

    Create Main.java
    hello github

---
+++ .github/workflows/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub! 这是我的第一个Java项目。");
+        System.out.println("当前时间: " + new java.util.Date());
+    }
+}
